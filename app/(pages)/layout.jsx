73548c2dfb3faf19c0958c7fa377c0a14ee2dd63
theknowledgepro <ajibopromise10@gmail.com>
2025-03-11 17:20:11 +0100

    Added Default Metadata

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Header, Footer } from '@/app/_components';
 
+export const metadata = { title: 'MGMOBI CARE' };
+
 export default function RootLayout({ children }) {
 	return (
 		<React.Fragment>
